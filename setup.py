5108e86cc0ea42c6017499ae8de3f3feef7946fc
Nithya TM <nithyatmn@gmail.com>
2025-05-14 01:04:02 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Mamatha',
+    author_email= 'mamatha2003s@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
